67e3e35e5f98b4a33998bf52bad7cb67924d2919
KarinSheri <karinsheri28@gmail.com>
2019-01-31 10:35:10 +0200

    NEW NETWORK

---
+++ models/network_4.js
@@ -0,0 +1,21 @@
+//Require Mongoose
+var mongoose = require('mongoose');
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var Network_4_Model = new Schema({
+  studentID         : {type: Schema.Types.ObjectId, ref: 'Students'},
+  time_sec          : {type: Number, required: true},
+  weight            : {type: Number, required: true},
+  node_1            : {type: Number, required: true},
+  node_2            : {type: Number, required: true},
+  node_3            : {type: Number, required: true},
+  node_4            : {type: Number, required: true},
+  node_5            : {type: Number, required: true},
+  node_6            : {type: Number, required: true},
+  node_7            : {type: Number, required: true}
+});
+
+//Export function to create "SomeModel" model class
+module.exports = mongoose.model('Network_4', Network_4_Model);
