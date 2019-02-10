8565ac40f3bc6e007ab620facbc39d8da693ab18
KarinSheri <karinsheri28@gmail.com>
2019-02-10 16:51:37 +0200

    remove titels

--- public/javascripts/net2-prop.js
+++ public/javascripts/net2-prop.js
@@ -35,7 +35,7 @@ var nodes = new vis.DataSet([
   {id: 14, x: 1400, y: 0}
 ]);*/
 
-// create an array with edges
+/*
 var edges = new vis.DataSet([
   {from: 1, to: 7, label: '1', title: '1'},
   {from: 1, to: 8, label: '2', title: '2'},
@@ -64,4 +64,35 @@ var edges = new vis.DataSet([
   {from: 6, to: 7, label: '1', title: '1'},
   {from: 6, to: 11, label: '5', title: '5'},
   {from: 6, to: 14, label: '8', title: '8'}
+]);*/
+
+// create an array with edges
+var edges = new vis.DataSet([
+  {from: 1, to: 7, label: '1'},
+  {from: 1, to: 8, label: '2'},
+  {from: 1, to: 9, label: '3'},
+  {from: 1, to: 10, label: '4'},
+  {from: 1, to: 12, label: '6'},
+  {from: 2, to: 7, label: '1'},
+  {from: 2, to: 8, label: '2'},
+  {from: 2, to: 9, label: '3'},
+  {from: 2, to: 10, label: '4'},
+  {from: 2, to: 12, label: '6'},
+  {from: 3, to: 7, label: '1'},
+  {from: 3, to: 8, label: '2'},
+  {from: 3, to: 9, label: '3'},
+  {from: 3, to: 10, label: '4'},
+  {from: 3, to: 13, label: '7'},
+  {from: 4, to: 7, label: '1'},
+  {from: 4, to: 8, label: '2'},
+  {from: 4, to: 9, label: '3'},
+  {from: 4, to: 11, label: '5'},
+  {from: 4, to: 13, label: '7'},
+  {from: 5, to: 7, label: '1'},
+  {from: 5, to: 8, label: '2'},
+  {from: 5, to: 11, label: '5'},
+  {from: 5, to: 14, label: '8'},
+  {from: 6, to: 7, label: '1'},
+  {from: 6, to: 11, label: '5'},
+  {from: 6, to: 14, label: '8'}
 ]);
