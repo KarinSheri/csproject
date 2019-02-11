b671c2b0d35893ea4dba9a71e4cae9e487bd21ec
KarinSheri <karinsheri28@gmail.com>
2019-02-11 17:25:37 +0200

    Network 2 changed to Noam Proposal

--- public/javascripts/net2-prop.js
+++ public/javascripts/net2-prop.js
@@ -66,6 +66,7 @@ var edges = new vis.DataSet([
   {from: 6, to: 14, label: '8', title: '8'}
 ]);*/
 
+// for Case 4
 /*var edges = new vis.DataSet([
   {from: 1, to: 7, label: '1'},
   {from: 1, to: 8, label: '2'},
@@ -96,8 +97,8 @@ var edges = new vis.DataSet([
   {from: 6, to: 14, label: '9'} //8
 ]);*/
 
-// create an array with edges
-var edges = new vis.DataSet([
+// create an array with edges --- WOrk for case 2 but too similar to optimal
+/*var edges = new vis.DataSet([
   {from: 1, to: 7, label: '1'},
   {from: 1, to: 8, label: '2'},
   {from: 1, to: 9, label: '3'},
@@ -125,4 +126,35 @@ var edges = new vis.DataSet([
   {from: 6, to: 7, label: '1'},
   {from: 6, to: 11, label: '5'},
   {from: 6, to: 14, label: '9'} //8
+]);*/
+
+// create an array with edges -- Changed according Noam
+var edges = new vis.DataSet([
+  {from: 1, to: 7, label: '5'},
+  {from: 1, to: 8, label: '4'},
+  {from: 1, to: 9, label: '3'},
+  {from: 1, to: 10, label: '2'},
+  {from: 1, to: 12, label: '1'},
+  {from: 2, to: 7, label: '5'},
+  {from: 2, to: 8, label: '4'},
+  {from: 2, to: 9, label: '3'},
+  {from: 2, to: 10, label: '2'},
+  {from: 2, to: 12, label: '1'},
+  {from: 3, to: 7, label: '5'},
+  {from: 3, to: 8, label: '4'},
+  {from: 3, to: 9, label: '3'},
+  {from: 3, to: 10, label: '2'},
+  {from: 3, to: 13, label: '1'},
+  {from: 4, to: 7, label: '5'},
+  {from: 4, to: 8, label: '4'},
+  {from: 4, to: 9, label: '3'},
+  {from: 4, to: 11, label: '2'},
+  {from: 4, to: 13, label: '1'},
+  {from: 5, to: 7, label: '5'},
+  {from: 5, to: 8, label: '4'},
+  {from: 5, to: 11, label: '2'},
+  {from: 5, to: 14, label: '1'},
+  {from: 6, to: 7, label: '5'},
+  {from: 6, to: 11, label: '2'},
+  {from: 6, to: 14, label: '1'} //8
 ]);
