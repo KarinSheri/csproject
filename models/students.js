ff2ddf6ab3f3820a6f499500f03174141b001397
KarinSheri <karinsheri28@gmail.com>
2019-03-11 21:51:26 +0200

    Master degree

--- models/students.js
+++ models/students.js
@@ -8,9 +8,9 @@ var StudentModel = new Schema({
   age               : {type: Number, required: true},
   gender            : {type: String, enum: ['Male', 'Female'], required: true},
   email             : {type: String, required: true},
-  education         : {type: String, enum: ['Less than high school',"High school","Bachelor","Master)",'Doctorate'], required: true},
+  education         : {type: String, enum: ["Less than high school","High school","Bachelor","Master","Doctorate"], required: true},
   industry          : {type: String, required: true},
-  date            : {type: Date, default: Date.now}
+  date              : {type: Date, default: Date.now}
   });
 
 //Export function to create "SomeModel" model class
